3a860b7a838bbfe0cb14d5d061cf43875e12836b
HP <accforproject495@gmail.com>
2022-05-30 08:02:42 +0530

    30052022

---
+++ index.js
@@ -0,0 +1,24 @@
+function onCrime(){
+    localStorage.setItem("topic","Crime");
+    window.location.href = "topic.html"
+}
+function onTriller(){
+    localStorage.setItem("topic","Thriller");
+    window.location.href = "topic.html"
+}
+function onHistory(){
+    localStorage.setItem("topic","Historical Fiction");
+    window.location.href = "topic.html"
+}
+function onLiter(){
+    localStorage.setItem("topic","Literary Fiction");
+    window.location.href = "topic.html"
+}
+function onRomance(){
+    localStorage.setItem("topic","Romance");
+    window.location.href = "topic.html"
+}
+function onPer(){
+    localStorage.setItem("topic","Personality Development");
+    window.location.href = "topic.html"
+}
