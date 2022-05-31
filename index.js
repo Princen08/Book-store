012be9b45dc8f76bf76cf2b5ac21e274f80eb933
HP <accforproject495@gmail.com>
2022-05-31 16:54:23 +0530

    31052022

--- index.js
+++ index.js
@@ -22,3 +22,17 @@ function onPer(){
     localStorage.setItem("topic","Personality Development");
     window.location.href = "topic.html"
 }
+function go(){
+    window.location.href="index.html";
+}
+function show(){
+    window.location.href="popup.html";
+    document.body.style.opacity = 0.2;
+    var orderno = Math.floor(Math.random() * 100000000) + 1;
+    document.getElementById("mes").style.opacity = 1;
+    document.getElementById("mes").innerHTML =` <div class="mess" style="background-color: aliceblue; margin-right:320pt; width: 500pt; height:180pt; border-radius: 10pt; margin-top: 250pt;">
+    <h3 style="text-align: center; padding-top:40pt;font-size:20pt">Thank you</h3>
+    <h3 style="text-align: center; padding-top:10pt; padding-left:20pt">Your order has been placed successfully.</h3>
+    <h3 style="text-align: center; padding-top:10pt; padding-left:20pt; padding-right:20pt">Your order number is ${orderno}. We will immediatelly process your and it will be delivered in 2 - 5 business days.</h3>
+</div>`
+}
